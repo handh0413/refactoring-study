da53f927b16fb24d5b058071fb3da26b8b9d0d5c
한동희 <pj.donghee.han@kalmate.net>
2024-07-22 22:09:39 +0900

    first commit

---
+++ src/main/java/jace/refactoring_study/RefactoringStudyApplication.java
@@ -0,0 +1,13 @@
+package jace.refactoring_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jace/refactoring_study/RefactoringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jace.refactoring_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
