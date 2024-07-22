a8b13c69540e9672ddfe6f09b1657127540df4bf
Donghee <handh0413@gmail.com>
2024-07-22 22:40:41 +0900

    NO-ISSUE Add sample code
    * 리팩토링 수행할 샘플 코드를 추가한다.

---
+++ src/main/java/jace/refactoring_study/chapter01/ex01/Customer.java
@@ -0,0 +1,66 @@
+package jace.refactoring_study.chapter01.ex01;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String name;
+    private Vector<Rental> rentals = new Vector<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration<Rental> enumeration = this.rentals.elements();
+        String result = "Rental Record for " + getName() + "\n";
+        while (enumeration.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = enumeration.nextElement();
+
+            // determine amounts for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDREN:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two days new release rental
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            // show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
+}
---
+++ src/main/java/jace/refactoring_study/chapter01/ex01/Rental.java
@@ -0,0 +1,19 @@
+package jace.refactoring_study.chapter01.ex01;
+
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
