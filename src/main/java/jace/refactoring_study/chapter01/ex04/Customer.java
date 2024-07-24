8f7454156697b78030cce094373182730d3d0eeb
Donghee <handh0413@gmail.com>
2024-07-24 19:06:47 +0900

    NO-ISSUE Refactoring with Replace Temp with Query
    * Replace Temp with Query 사용하여 임시변수를 제거한다.

--- src/main/java/jace/refactoring_study/chapter01/ex04/Customer.java
+++ src/main/java/jace/refactoring_study/chapter01/ex04/Customer.java
@@ -20,21 +20,36 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration<Rental> enumeration = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (enumeration.hasMoreElements()) {
             Rental each = enumeration.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
-            totalAmount += each.getCharge();
         }
 
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        result += "Amount owed is " + getTotalCharge() + "\n";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration<Rental> enumeration = rentals.elements();
+        while (enumeration.hasMoreElements()) {
+            Rental each = enumeration.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration<Rental> enumeration = rentals.elements();
+        while (enumeration.hasMoreElements()) {
+            Rental each = enumeration.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
         return result;
     }
 }
