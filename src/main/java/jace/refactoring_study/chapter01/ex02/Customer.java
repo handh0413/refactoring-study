636dcb4d79e07e75c4851dcb2d9bc338c153aaf6
Donghee <handh0413@gmail.com>
2024-07-24 18:38:38 +0900

    NO-ISSUE Refactoring with Method Extract
    * Method Extract 이용하여 리팩토링을 수행한다 (패키지: ex01 -> ex02)

---
+++ src/main/java/jace/refactoring_study/chapter01/ex02/Customer.java
@@ -0,0 +1,69 @@
+package jace.refactoring_study.chapter01.ex02;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String name;
+    private Vector<Rental> rentals = new Vector<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration<Rental> enumeration = this.rentals.elements();
+        String result = "Rental Record for " + getName() + "\n";
+        while (enumeration.hasMoreElements()) {
+            Rental each = enumeration.nextElement();
+            double thisAmount = amountFor(each);
+
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two days new release rental
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            // show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
+
+    private double amountFor(Rental each) {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2) {
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3) {
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
+}
