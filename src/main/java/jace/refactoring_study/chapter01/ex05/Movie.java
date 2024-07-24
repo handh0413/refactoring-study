eafdea7380f03bf3921a6270f71c971dc8bba975
Donghee <handh0413@gmail.com>
2024-07-24 19:33:21 +0900

    NO-ISSUE Refactoring with Move Method
    * 관련 기능을 제공하는 메소드를 적절한 클래스로 이동시킨다. (패키지: ex04 -> ex05)

---
+++ src/main/java/jace/refactoring_study/chapter01/ex05/Movie.java
@@ -0,0 +1,53 @@
+package jace.refactoring_study.chapter01.ex05;
+
+public class Movie {
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    public static final int CHILDREN = 2;
+
+    private String title;
+    private int priceCode;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
+    }
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+}
